69ff32267e241482c6324499486afb6d36879e72
Rj Saini <rsaini88@gmail.com>
2023-02-05 23:15:33 -0800

    userId error

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,4 +1,3 @@
-
 // Dashboard Routes
 // This is a set of routes that will be used to render the dashboard pages.
 // All of these routes will be protected by the withAuth middleware function.
@@ -17,11 +16,25 @@ router.get("/", withAuth, async (req, res) => {
   // refer to admin-all-posts.handlebars write the code to display the posts
 });
 
+router.get("/create", withAuth, async (req, res) => {
+  res.render("admin-create-post", { laytout: "dashboard" });
+});
 // TODO - create logic for the GET route for /new that renders the new post page
 // It should display a form for creating a new post
+router.post("/create", withAuth, async (req, res) => {
+  try {
+    await Post.create({
+      title: req.body.title,
+      body: req.body.body,
+      userId: req.body.userId,
+    });
+    res.status(200).json({ message: "Post created successfully" });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // TODO - create logic for the GET route for /edit/:id that renders the edit post page
 // It should display a form for editing an existing post
 
 module.exports = router;
-
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -6,6 +6,10 @@ const { Post, Comment, User } = require("../models/");
 // TODO - work on GET route for getting all posts
 // this page can be viewed without logging in
 router.get("/", async (req, res) => {
+  let username;
+  if (req.session.loggedIn) {
+    username = req.session.username;
+  }
   // TODO - retrieve all posts from the database
   // render the homepage template with the posts retrieved from the database
   // refer to homepage.handlebars write the code to display the posts
