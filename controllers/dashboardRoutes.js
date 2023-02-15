f0db90ac6ef021f11fb5b402075c4dbb7471ea27
Rj Saini <rsaini88@gmail.com>
2023-02-15 13:50:09 -0800

    added dashboard routes

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -19,12 +19,12 @@ router.get("/", withAuth, async (req, res) => {
       },
     ],
   });
-  const posts = postsData.map((post) => post.get({ plain: true }));
   // TODO - retrieve all posts from the database for the logged in user
   // render the dashboard template with the posts retrieved from the database
   //default layout is set to main.handlebars, layout need to be changed to dashboard to use dashboard.handlebars
-  res.render("admin-all-posts", { layout: "dashboard", posts });
   // refer to admin-all-posts.handlebars write the code to display the posts
+  const posts = postsData.map((post) => post.get({ plain: true }));
+  res.render("admin-all-posts", { layout: "dashboard", posts });
 });
 
 router.get("/create", withAuth, async (req, res) => {
@@ -32,21 +32,43 @@ router.get("/create", withAuth, async (req, res) => {
 });
 // TODO - create logic for the GET route for /new that renders the new post page
 // It should display a form for creating a new post
-router.post("/create", withAuth, async (req, res) => {
-  try {
-    await Post.create({
-      title: req.body.title,
-      body: req.body.body,
-      userId: req.session.userId,
-    });
-    res.redirect("/dashboard");
-    // res.status(200).json({ message: "Post created successfully" });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+
+router.get("/new", withAuth, async (req, res) => {
+  res.render("admin-create-post", { layout: "dashboard" });
 });
+
+// router.post("/create", withAuth, async (req, res) => {
+//   try {
+//     await Post.create({
+//       title: req.body.title,
+//       body: req.body.body,
+//       userId: req.session.userId,
+//     });
+//     res.redirect("/dashboard");
+//     // res.status(200).json({ message: "Post created successfully" });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 // TODO - create logic for the GET route for /edit/:id that renders the edit post page
 // It should display a form for editing an existing post
+router.get("edit/:id", async (req, rest) => {
+  try {
+    const singlePost = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ["id"],
+        },
+      ],
+    });
+    const editPost = singlePost.get({ plain: true });
+    res.render("admin-create-post", { layout: "dashboard", singlePost });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
