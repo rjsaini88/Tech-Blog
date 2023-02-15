e40c80f903c6639cce63025baf9691fb7e5a1109
Rj Saini <rsaini88@gmail.com>
2023-02-15 01:27:22 -0800

    completed comment and post routes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,10 +1,20 @@
-
-const router = require('express').Router();
-const { Comment } = require('../../models/');
-const withAuth = require('../../utils/auth');
+const router = require("express").Router();
+const { Comment } = require("../../models/");
+const withAuth = require("../../utils/auth");
 
 // TODO - create a POST route for creating a new comment
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const newComment = await Comment.create({
+      ...req.body, //example from mini project if doesn't work try: body: req.body.body,
+      user_id: req.session.user_id,
+    });
+    res.status(200).json(newComment);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 // This should be a protected route, so you'll need to use the withAuth middleware
 
 module.exports = router;
-
--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,17 +1,59 @@
-
-const router = require('express').Router();
-const { Post } = require('../../models/');
-const withAuth = require('../../utils/auth');
+const router = require("express").Router();
+const { Post } = require("../../models/");
+const withAuth = require("../../utils/auth");
 
 // TODO - create a POST route for creating a new post
 // This should be a protected route, so you'll need to use the withAuth middleware
-
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const newPost = await Post.create({
+      title: req.body.title,
+      body: req.body.body,
+      user_id: req.session.user_id,
+    });
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // TODO - create a PUT route for updating a post's title or body
 // This should be a protected route, so you'll need to use the withAuth middleware
+router.put("/update/:id", withAuth, async (req, res) => {
+  try {
+    const updatePost = await Post.update(
+      {
+        title: req.body.title,
+        body: req.body.body,
+      },
+      {
+        where: { id: req.params.id },
+      }
+    );
 
+    if (!updatePost) {
+      res.status(404).json({ message: "Invalid post id!!!" });
+    }
+    res.status(200).json("Your post have been updated", updatePost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // TODO - create a DELETE route for deleting a post with a specific id
 // This should be a protected route, so you'll need to use the withAuth middleware
+router.delete("/delete/:id", withAuth, async (req, res) => {
+  try {
+    const deletePost = await Post.destroy({
+      where: { id: req.params.id },
+    });
+    if (!deletePost) {
+      res.status(404).json({ message: "Invalid post id!!!" });
+    }
+    res.status(200).json("Your post have been deleted", deletePost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
